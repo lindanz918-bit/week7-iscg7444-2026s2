a19386cf9ee2bc5a81130013ae13e85c9c5d001f
lindanz918-bit <lindanz918@gmail.com>
2026-09-11 14:52:55 +1200

    aaa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Required for Docker container mapping
+    port: 3000,      // Specifies the internal port
+    strictPort: true // Prevents Vite from auto-switching ports if 5173 is busy
+  }
+
 })
